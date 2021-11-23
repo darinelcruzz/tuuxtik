93a9b526521902a08eaecb03cc3f51631f294c26
Darinel Cruz <darinelcruzz@gmail.com>
2021-11-23 12:27:14 -0600

    Website ready

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#334571',
+        'secondary': '#A5D7D5',
+      },
+      container: {
+        padding: '0.5rem'
+      },
+      fontFamily: {
+        noto: "'Noto Sans', sans-serif",
+        poppins: "'Poppins', sans-serif",
+        roboto: "'Roboto', sans-serif",
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
